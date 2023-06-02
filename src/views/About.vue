d90155b17ad1122ccbdbad94efa91052ab7a0481
zhangjiangfeng <2826758674@qq.com>
2023-06-02 17:13:20 +0800

    feat(update view): update views text
    update views text

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p>test to v0.3.0, 再重新生成 changelog</p>
+    <p>test to v0.3.0, 再重新生成 changelog 测试 </p>
     <p></p>
   </div>
 </template>
